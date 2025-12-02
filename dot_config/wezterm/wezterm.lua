0dc0b61bd21c28f5ef15ff8765cf25f5251e8bd0
saigo57 <a.saigo1997@gmail.com>
2025-12-03 04:08:02 +0900

    weztermをtmuxの代わりにする設定を追加

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,11 +7,26 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'AdvenureTime'
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 config.font_size = 10
+config.scrollback_lines = 10000
+config.enable_scroll_bar = true
+config.colors = {
+  tab_bar = {
+    active_tab = {
+      bg_color = "#6194aa",
+      fg_color = "#ffffff",
+    },
+    inactive_tab = {
+      bg_color = "#303030",
+      fg_color = "#808080",
+    },
+  }
+}
 
 if is_windows then
-  config.default_prog = { 'pwsh.exe', '-NoLogo' }
+  config.default_domain = 'WSL:Ubuntu-24.04'
 else
   config.default_prog = { 'zsh', '-l' }
 end
@@ -28,6 +43,12 @@ config.window_background_opacity = 0.85
 --     window:gui_window():toggle_fullscreen()
 -- end)
 
+config.leader = {
+  key = "n",
+  mods = "CTRL",
+  timeout_milliseconds = 2000,
+}
+
 -- ショートカットキー設定
 local act = wezterm.action
 config.keys = {
@@ -36,6 +57,90 @@ config.keys = {
     key = 'f',
     mods = 'SHIFT|META',
     action = wezterm.action.ToggleFullScreen,
+  },
+  -- create a new tab
+  {
+    key = "c",
+    mods = "LEADER",
+    action = act.SpawnTab "DefaultDomain",
+  },
+  -- splitting
+  {
+    key = "s",
+    mods = "LEADER",
+    action = act.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "v",
+    mods = "LEADER",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  -- pane move
+  {
+    key = "l",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection "Right",
+  },
+  {
+    key = "h",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection "Left",
+  },
+  {
+    key = "j",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection "Down",
+  },
+  {
+    key = "k",
+    mods = "LEADER",
+    action = act.ActivatePaneDirection "Up",
+  },
+  -- move to tab
+  {
+    key = "1",
+    mods = "LEADER",
+    action = act.ActivateTab(0),
+  },
+  {
+    key = "2",
+    mods = "LEADER",
+    action = act.ActivateTab(1),
+  },
+  {
+    key = "3",
+    mods = "LEADER",
+    action = act.ActivateTab(2),
+  },
+  {
+    key = "4",
+    mods = "LEADER",
+    action = act.ActivateTab(3),
+  },
+  {
+    key = "5",
+    mods = "LEADER",
+    action = act.ActivateTab(4),
+  },
+  {
+    key = "6",
+    mods = "LEADER",
+    action = act.ActivateTab(5),
+  },
+  {
+    key = "7",
+    mods = "LEADER",
+    action = act.ActivateTab(6),
+  },
+  {
+    key = "8",
+    mods = "LEADER",
+    action = act.ActivateTab(7),
+  },
+  {
+    key = "9",
+    mods = "LEADER",
+    action = act.ActivateTab(8),
   },
 }
 
